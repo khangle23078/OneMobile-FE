a2d3928e738d9587675e689b30eb763e9b6a1a84
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-12-04 23:41:28 +0700

    style: add icon

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,7 +1,8 @@
 import { adminMenus } from '@/configs/AdminMenu';
-import { Layout, Menu, Breadcrumb, theme } from 'antd';
+import { Layout, Menu, Breadcrumb, theme, Button } from 'antd';
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import Logo from './../assets/OneMobile.png'
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -14,11 +15,17 @@ const AdminLayout = () => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className="demo-logo-vertical" />
+        <div className='flex items-center justify-center mx-1'>
+          <img src={Logo} width={100} height={100} />
+        </div>
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={adminMenus} />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Header style={{ padding: 0, background: colorBgContainer }} className='flex items-center justify-end'>
+          <Link to={'/'}>
+            <Button type='primary' className='mx-4'>Về trang chủ</Button>
+          </Link>
+        </Header>
         <Content style={{ margin: '0 16px' }}>
           <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>User</Breadcrumb.Item>
