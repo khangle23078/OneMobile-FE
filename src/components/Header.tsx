211607c75be43e4b8e5df97c05d9fc6b630d9e27
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-12-04 19:03:45 +0700

    style : finish header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,46 @@
-import { Link, NavLink } from 'react-router-dom';
-import Logo from './../assets/OneMobile.png'
-import { ShoppingCartOutlined } from '@ant-design/icons'
+import { MenuOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Badge, Button, Dropdown, Input, MenuProps } from 'antd';
+import { Link } from 'react-router-dom';
+import Logo from './../assets/OneMobile.png';
+import { useGetCategoriesQuery } from '@/app/services/category';
+import { Category } from '@/interfaces/product';
+
+const { Search } = Input;
 
 const Header: React.FC = () => {
-  return <header className='h-[80px] shadow-md'>
+  const { data: response } = useGetCategoriesQuery();
+
+  const items: MenuProps['items'] = response?.data.map((category: Category, index: number) => {
+    return {
+      label: (
+        <Link to={`/category/${category._id}`}>{category.name}</Link>
+      ),
+      key: `${index + 1}`
+    }
+  })
+
+  return <header className='h-[100px] shadow-md'>
     <div className='h-screen mx-auto max-w-7xl'>
-      <div className='flex items-center justify-between'>
-        <Link to={'/'}>
-          <img src={Logo} width={100} height={100} />
-        </Link>
-        <nav>
-          <ul className='flex items-center gap-3'>
-            <li className='list-none'>
-              <NavLink to="" className='text-black no-underline'>Trang chủ</NavLink>
-            </li>
-            <li className='list-none'>
-              <NavLink to="" className='text-black no-underline'>Sản phẩm</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <ShoppingCartOutlined className='text-3xl' />
+      <div className='flex items-center justify-around'>
+        <div className='flex items-center gap-4'>
+          <Link to={'/'}>
+            <img src={Logo} width={100} height={100} />
+          </Link>
+          <Dropdown menu={{ items }} trigger={['click']}>
+            <Button type='dashed' icon={<MenuOutlined />}>Danh mục</Button>
+          </Dropdown>
+        </div>
+        <Search placeholder='nhập tên sản phẩm' enterButton className='w-[600px]' />
+        <div className='flex items-center gap-4'>
+          <Link to={'/cart'}>
+            <Badge count={1}>
+              <ShoppingCartOutlined className='text-3xl' />
+            </Badge>
+          </Link>
+          <Link to={'/login'}>
+            <Button type='primary'>Đăng nhập</Button>
+          </Link>
+        </div>
       </div>
     </div>
   </header>;
