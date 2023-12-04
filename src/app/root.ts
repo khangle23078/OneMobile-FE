f516ae560d4b5e7d83e733ff79e0a36181e49cb4
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-12-04 19:04:13 +0700

    style : add more style  header component

--- src/app/root.ts
+++ src/app/root.ts
@@ -1,8 +1,10 @@
 import authSlice from "@/features/authSlice";
 import { combineReducers } from "redux";
 import { api } from "./services/api";
+import cartSlice from "@/features/cartSlice";
 
 export const rootReducer = combineReducers({
   [api.reducerPath]: api.reducer,
   auth: authSlice,
+  cart: cartSlice
 })
---
+++ src/features/cartSlice.ts
@@ -0,0 +1,25 @@
+import { Product } from '@/interfaces/product';
+import { createSlice } from '@reduxjs/toolkit'
+
+
+interface CartState {
+  products: Product[],
+  quantity: number
+}
+
+const initialState = {
+
+} as CartState
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, { payload }) => {
+    }
+  }
+});
+
+export const { addToCart } = cartSlice.actions
+
+export default cartSlice.reducer
